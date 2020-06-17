7a1ccc691297f9c041201eeac66dc141c80e8cfb
Surefirer <46170182+Surefirer@users.noreply.github.com>
2020-06-17 09:43:34 -0700

    1. change global font family. 2. header and hero background change. 3. patch news section mock up complete

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,15 +1,36 @@
 import React from "react";
+import Clock from "react-live-clock";
+import newsImg from "../../assets/images/news.png";
 
 function Home() {
   return (
     <div className="main">
       <div className="main__hero">
-        <p>clock</p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi qui
-          laudantium in, iure placeat ullam ab odit delectus facere aliquam.
-        </p>
+        <div className="main__time">
+          <Clock format={"HH:mm:ss"} ticking={true} timezone={"US/Pacific"} />
+        </div>
+        <div className="main__intro">
+          <p>
+            YYRO is a Renewal RO private server dedicated to bring players
+            together in an active, friendly community.
+          </p>
+          <p> Download it today!</p>
+        </div>
       </div>
+      <a href="http://yyragnarok.com/forum.php?mod=viewthread&tid=459&extra=page%3D1">
+        <div className="news">
+          <div className="news__title">
+            <h1>LATEST NEWS</h1>
+          </div>
+          <div className="news__cover">
+            <img src={newsImg} alt="" />
+          </div>
+          <div className="news__sub">
+            <h2>Jun 7,2020</h2>
+            <h3 className="news__pn">Patch Notes</h3>
+          </div>
+        </div>
+      </a>
     </div>
   );
 }
