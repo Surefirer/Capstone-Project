f4b780e51c4448ac9010b1cc8902313dd2af4ab0
Surefirer <46170182+Surefirer@users.noreply.github.com>
2020-06-17 18:29:32 -0700

    header add scroll animation

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,26 +3,45 @@ import { Logos } from "./assets";
 import DrawerToggleButton from "../components/sideDrawer/DrawerToggleButton";
 //import { NavLink, Link } from "react-router-dom";
 
-function Header(props) {
-  return (
-    <>
-      <div className="nav-bar">
-        <div>
-          <DrawerToggleButton click={props.drawerClickHandler} />
-        </div>
-        <div className="nav-bar__logo">
-          <img src={Logos.logo} alt="yyro logo" />
-        </div>
+// function Header(props) {
+class Header extends React.Component {
+  componentDidMount() {
+    window.addEventListener("scroll", () => {
+      const isTop = window.scrollY > 100;
+      const nav = document.getElementById("nav");
+      if (isTop) {
+        nav.classList.add("scrolled");
+      } else {
+        nav.classList.remove("scrolled");
+      }
+    });
+  }
 
-        <ul className="nav-bar__menu">
-          <li>Information</li>
-          <li>Download</li>
-          <li>Forum</li>
-          <li>Database</li>
-        </ul>
-      </div>
-    </>
-  );
+  componentWillUnmont() {
+    window.removeEventListener("scroll");
+  }
+
+  render() {
+    return (
+      <>
+        <div id="nav" className="nav-bar">
+          <div>
+            <DrawerToggleButton click={this.props.drawerClickHandler} />
+          </div>
+          <div className="nav-bar__logo">
+            <img src={Logos.logo} alt="yyro logo" />
+          </div>
+
+          <ul className="nav-bar__menu">
+            <li>Information</li>
+            <li>Download</li>
+            <li>Forum</li>
+            <li>Database</li>
+          </ul>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Header;
