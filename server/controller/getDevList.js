e86db09e679ca76b04376fd2ffb1f3a88a66d5ca
Surefirer <46170182+Surefirer@users.noreply.github.com>
2020-06-23 19:31:27 -0700

    fix developer and gm info not showing in the staff page

--- server/controller/getDevList.js
+++ server/controller/getDevList.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
+const path = require("path");
 
 const getDevList = () => {
-  const DevData = fs.readFileSync("./model/staffs/developers.json");
+  const DevData = fs.readFileSync(
+    path.resolve(__dirname, "../model/staffs/developers.json")
+  );
   const DevArr = JSON.parse(DevData);
   return DevArr;
 };
--- server/controller/getGMList.js
+++ server/controller/getGMList.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
+const path = require("path");
 
 const getGMList = () => {
-  const GMData = fs.readFileSync("./model/staffs/gms.json");
+  const GMData = fs.readFileSync(
+    path.resolve(__dirname, "../model/staffs/gms.json")
+  );
   const GMArr = JSON.parse(GMData);
   return GMArr;
 };
