f10a5fdeea59148caef15e3b0c2acf77f5a668f6
Surefirer <46170182+Surefirer@users.noreply.github.com>
2020-06-23 17:00:08 -0700

    refactor before dep to heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const app = express();
+app.use(express.static("public"));
 
 const getAdministratorList = require("./controller/getAdministratorList");
 const getDevList = require("./controller/getDevList");
@@ -15,6 +17,9 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
+
+// serve static files, create react app build
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // api info endpoint for admin
 app.get("/administrator", (req, res) => {
@@ -39,6 +44,11 @@ app.get("/review", (req, res) => {
 // api info endpoint for dev
 app.get("/patchs", (req, res) => {
   res.json(getPatchList());
+});
+
+// All remaining requests return the React app, so it can handle routing.
+app.get("*", function (_req, res) {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
